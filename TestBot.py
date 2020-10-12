e9999a19ae9aff78c9cd9509a238854d9807d3cb
Kirill <kirisya228@gmail.com>
2020-10-13 01:03:55 +0300

    heroku

--- TestBot.py
+++ TestBot.py
@@ -11,7 +11,13 @@ class States(Enum):
 bot = telebot.TeleBot('1285966353:AAEIQ7RYIqx9rcV0Fm6om5RZeRSKy70Xpgc')
 players = []
 boss = 0
-current_states = States.NO_GAME
+current_states = [States.NO_GAME]
+
+def get_state():
+    return current_states[0]
+
+def set_state(state):
+    current_states[0] = state
 
 def generate_markup():
     """
@@ -43,31 +49,32 @@ def get_text_messages(message):
 
         #markup = generate_markup()
         bot.send_message(message.chat.id, 'Начат сбор игроков')
-        current_states.self = States.FIND_PLAYERS
+        set_state(States.FIND_PLAYERS)
         players.append(message.from_user.username)
     elif message.text == "/join":
-        if current_states.self == States.FIND_PLAYERS & message.from_user.username not in players:
+        if get_state() == States.FIND_PLAYERS & message.from_user.username not in players:
             players.append(message.from_user.username)
             bot.send_message(message.chat.id, 'Игрок ' + message.from_user.username + ' добавлен к игре')
     elif message.text == "/showPlayers":
-        if current_states.self == States.FIND_PLAYERS:
+        if get_state() == States.FIND_PLAYERS:
             text = ""
             i = 1
-            for j in players :
-                text += i + ". " + j + "\n"
-                i += 1
-            bot.send_message(message.chat.id, text)
+            if len(players) != 0:
+                for j in players:
+                    text += str(i) + ". " + str(j) + "\n"
+                    i += 1
+                bot.send_message(message.chat.id, text)
     elif message.text == "/startGame":
-        if current_states.self == States.FIND_PLAYERS and message.from_user.username == players[0]:
+        if get_state() == States.FIND_PLAYERS and message.from_user.username == players[0]:
             text = ""
             i = 1
             for j in players:
                 text += i + ". " + j + "\n"
                 i += 1
             bot.send_message(message.chat.id, text)
-            current_states.self = States.NO_GAME
+            set_state(States.NO_GAME)
     elif message.text == "/state":
-        bot.send_message(message.chat.id, "state " + current_states.self)
+        bot.send_message(message.chat.id, "state " + str(get_state().name))
 
 if __name__ == '__main__':
     bot.polling(none_stop=True, interval=0)
