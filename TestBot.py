4a0c40082871b680a781149cf9043dc2c78f6ad2
Kirill <kirisya228@gmail.com>
2020-10-12 22:23:47 +0300

    heroku

--- TestBot.py
+++ TestBot.py
@@ -33,28 +33,27 @@ def get_text_messages(message):
         bot.send_message(message.chat.id, "Привет. Тут пока идёт стройка. Возвращайся позже!")
     elif message.text == "/game":
         markup = generate_markup()
-        bot.send_message(message.chat.id, 'Угадай число', reply_markup=markup)
-        check_answer()
-    else:
-        bot.send_message(message.chat.id, "спам")
+        bot.send_message(message.chat.id, 'game', reply_markup=markup)
+    #else:
+        #bot.send_message(message.chat.id, "спам")
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def check_answer(message):
-    # Если функция возвращает None -> Человек не в игре
+    ## Если функция возвращает None -> Человек не в игре
     answer = str(random.randint(1, 3))
-    # Как Вы помните, answer может быть либо текст, либо None
-    # Если None:
-    if not answer:
-        bot.send_message(message.chat.id, 'Чтобы начать игру, выберите команду /game')
-    else:
-        # Уберем клавиатуру с вариантами ответа.
-        keyboard_hider = telebot.types.ReplyKeyboardRemove()
-        # Если ответ правильный/неправильный
-        if message.text == answer:
-            bot.send_message(message.chat.id, 'Верно!', reply_markup=keyboard_hider)
-        else:
-            bot.send_message(message.chat.id, 'Увы, Вы не угадали. Попробуйте ещё раз! Ответ: ' + answer,
-                             reply_markup=keyboard_hider)
+    ## Как Вы помните, answer может быть либо текст, либо None
+    ## Если None:
+    #if not answer:
+    #    bot.send_message(message.chat.id, 'Чтобы начать игру, выберите команду /game')
+    #else:
+    #    # Уберем клавиатуру с вариантами ответа.
+    #    keyboard_hider = telebot.types.ReplyKeyboardRemove()
+    #    # Если ответ правильный/неправильный
+    #    if message.text == answer:
+    #        bot.send_message(message.chat.id, 'Верно!', reply_markup=keyboard_hider)
+    #    else:
+    #        bot.send_message(message.chat.id, 'Увы, Вы не угадали. Попробуйте ещё раз! Ответ: ' + answer,
+    #                         reply_markup=keyboard_hider)
 
 
 if __name__ == '__main__':
