5759a8a587ec622649b01fba58b606c325168974
Kirill <kirisya228@gmail.com>
2020-10-14 14:34:17 +0300

    refactor all commands

--- TestBot.py
+++ TestBot.py
@@ -70,6 +70,8 @@ def get_text_messages(message):
         else:
             text = "Пустая команда для миссии"
         bot.send_message(message.chat.id, text)
+    elif get_state() == States.NO_GAME:
+        bot.send_message(message.chat.id, "Игра не начата")
 
 @bot.message_handler(commands=['help'])
 def get_text_messages(message):
@@ -82,26 +84,25 @@ def get_text_messages(message):
                                                              "/team для показа текущей команды для миссии\n"
                                                              "/state для показа текущего состояния")
 
-@bot.message_handler(content_types=['text'], func=lambda message: message.chat.id == 446193106)
+@bot.message_handler(commands=['create'])
 def get_text_messages(message):
-    if message.text == "/create":
+    if message.chat.id == 446193106:
         bot.send_message(message.chat.id, 'Используй эту команду в групповом чате')
-
-@bot.message_handler(content_types=['text'], func=lambda message: message.chat.id != 446193106 and get_state() == States.NO_GAME)
-def get_text_messages(message):
-    if message.text == "/create":
-        #markup = generate_markup()
+    elif get_state() == States.NO_GAME:
         bot.send_message(message.chat.id, 'Начат сбор игроков')
         set_state(States.FIND_PLAYERS)
         players.append(message.from_user.username)
 
-@bot.message_handler(content_types=['text'], func=lambda message: message.chat.id != 446193106 and get_state() == States.FIND_PLAYERS)
+@bot.message_handler(commands=['join'])
 def get_text_messages(message):
-    if message.text == "/join":
+    if message.chat.id != 446193106 and get_state() == States.FIND_PLAYERS:
         if message.from_user.username not in players:
             players.append(message.from_user.username)
             bot.send_message(message.chat.id, 'Игрок ' + str(message.from_user.username) + ' добавлен к игре')
-    elif message.text == "/startGame":
+
+@bot.message_handler(commands=['startGame'])
+def get_text_messages(message):
+    if message.chat.id != 446193106 and get_state() == States.FIND_PLAYERS:
         if message.from_user.username == players[0]:
             text = ""
             i = 1
@@ -125,37 +126,13 @@ def get_callback_btn(callback_query: telebot.types.CallbackQuery):
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "/showPlayers":
-        if get_state() == States.FIND_PLAYERS and message.from_user.username == "kirisya228":
-            text = ""
-            i = 1
-            if len(players) != 0:
-                for j in players:
-                    text += str(i) + ". @" + str(j) + "\n"
-                    i += 1
-                bot.send_message(message.chat.id, text)
-        elif get_state() == States.NO_GAME:
-            bot.send_message(message.chat.id, "Игра не начата")
-    elif message.text == "/endGame":
+    if message.text == "/endGame":
         if get_state() != States.NO_GAME and message.from_user.username in players:
             players.clear()
             boss.clear()
             mission_composition.clear()
             set_state(States.NO_GAME)
             bot.send_message(message.chat.id, "Игра оконченна")
-    elif message.text == "/state" and message.from_user.username == "kirisya228":
-        bot.send_message(message.chat.id, "state " + str(get_state().name))
-    elif message.text == "/showCommand":
-        if get_state() == States.SET_MISSION_СOMPOSITION and message.from_user.username == "kirisya228":
-            text = ""
-            i = 1
-            if len(mission_composition) != 0:
-                for j in mission_composition:
-                    text += str(i) + ". @" + str(j) + "\n"
-                    i += 1
-                bot.send_message(message.chat.id, text)
-    elif message.text == "/state" and message.from_user.username == "kirisya228":
-        bot.send_message(message.chat.id, "state " + str(get_state().name))
 
 
 if __name__ == '__main__':
